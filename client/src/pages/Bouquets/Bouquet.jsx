27b77515aede6308ac9eae35457061b3ca35d042
kaberi13 <kaberimushahary645@gmail.com>
2025-07-04 22:46:04 +0530

    Bouquet page updated

--- client/src/pages/Bouquets/Bouquet.jsx
+++ client/src/pages/Bouquets/Bouquet.jsx
@@ -1,4 +1,4 @@
-
+import { useState } from 'react';
 import './Bouquet.css';
 import '../../components/Card/Card.css';
 import '../../components/Footer/Footer.css';
@@ -18,23 +18,88 @@ import img7 from '../../assets/Bouquets/mixedbouquet.jpg';
 import img8 from '../../assets/Bouquets/RedRose.jpg';
 
 function Bouquet() {
+  const [wishlist, setWishlist] = useState([]);
+
+  const handleWishlistToggle = (itemData) => {
+    if (itemData.isWishlisted) {
+      setWishlist(prev => [...prev, itemData]);
+      console.log(`Added ${itemData.tittle} to wishlist`);
+    } else {
+      setWishlist(prev => prev.filter(item => item.tittle !== itemData.tittle));
+      console.log(`Removed ${itemData.tittle} from wishlist`);
+    }
+  };
+
   return (
     <>
       <Header />
       <Navbar />
       <h1>Crochet Flower</h1>
 
-      <div className='card-container'>
-        <Card image={img1} title="Sunflower" price="300" link="/categories/bouquet/Sunflower" />
-        <Card image={img2} title="Pink and White Rose" price="350" link="/categories/bouquet/PinkWhite" />
-        <Card image={img3} title="Rose Bouquet" price="320" link="/categories/bouquet/RoseBouquet" />
-        <Card image={img4} title="Double Layered Rose" price="250" link="/categories/bouquet/DoubleLayeredRose" />
-        <Card image={img5} title="Tulip, Rose" price="100" link="/categories/bouquet/TulipRose" />
-        <Card image={img6} title="Blue Rose" price="200" link="/categories/bouquet/BlueRose" />
-        <Card image={img7} title="Mixed Bouquet" price="300" link="/categories/bouquet/MixedBouquet" />
-        <Card image={img8} title="Red Rose" price="150" link="/categories/bouquet/RedRose" />
-      </div>
+      {wishlist.length > 0 && (
+        <div className="wishlist-info">
+          <p>❤️ {wishlist.length} item{wishlist.length > 1 ? 's' : ''} in your wishlist</p>
+        </div>
+      )}
 
+      <div className='card-container'>
+        <Card
+          image={img1}
+          tittle="Sunflower"
+          price="300"
+          onWishlistToggle={handleWishlistToggle}
+          link="/categories/bouquet/Sunflower"
+        />
+        <Card
+          image={img2}
+          tittle="Pink and White Rose"
+          price="350"
+          onWishlistToggle={handleWishlistToggle}
+          link="/categories/bouquet/PinkWhite"
+        />
+        <Card
+          image={img3}
+          tittle="Rose Bouquet"
+          price="320"
+          onWishlistToggle={handleWishlistToggle}
+          link="/categories/bouquet/RoseBouquet"
+        />
+        <Card
+          image={img4}
+          tittle="Double Layered Rose"
+          price="250"
+          onWishlistToggle={handleWishlistToggle}
+          link="/categories/bouquet/DoubleLayeredRose"
+        />
+        <Card
+          image={img5}
+          tittle="Tulip, Rose"
+          price="100"
+          onWishlistToggle={handleWishlistToggle}
+          link="/categories/bouquet/TulipRose"
+        />
+        <Card
+          image={img6}
+          tittle="Blue Rose"
+          price="200"
+          onWishlistToggle={handleWishlistToggle}
+          link="/categories/bouquet/BlueRose"
+        />
+        <Card
+          image={img7}
+          tittle="Mixed Bouquet"
+          price="300"
+          onWishlistToggle={handleWishlistToggle}
+          link="/categories/bouquet/MixedBouquet"
+        />
+        <Card
+          image={img8}
+          tittle="Red Rose"
+          price="150"
+          onWishlistToggle={handleWishlistToggle}
+          link="/categories/bouquet/RedRose"
+        />
+      </div>
       <Footer />
     </>
   );
