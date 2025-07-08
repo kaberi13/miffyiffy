7cf89c1237513800de5a7b9eefe389194f7a3cd4
jilmil12 <jilmil794@gmail.com>
2025-07-08 23:35:59 +0530

    Resolved merge conflict in Bracelet.jsx

--- client/src/pages/Bracelet/Bracelet.jsx
+++ client/src/pages/Bracelet/Bracelet.jsx
@@ -26,23 +26,13 @@ function Bracelet() {
         <Card
           image={img1}
           tittle="Star bracelet"
-<<<<<<< HEAD
-          price="200"
-          onWishlistToggle={handleWishlistToggle}
-=======
           price="220"
->>>>>>> cd6334f1e8ba4a00363350fd6a8cd9d2b6c05226
           link="/categories/bracelet/StarBracelet" // ✅ Link to detail page
         />
         <Card
           image={img2}
           tittle="Blue bracelet"
-<<<<<<< HEAD
-          price="200"
-          onWishlistToggle={handleWishlistToggle}
-=======
           price="260"
->>>>>>> cd6334f1e8ba4a00363350fd6a8cd9d2b6c05226
           link="/categories/bracelet/BlueBracelet" // ✅ Link to detail page
         />
       </div>
